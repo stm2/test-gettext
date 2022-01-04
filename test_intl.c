f672600011da5e8e774ddd75a4737ca7556881fa
Steffen Mecke <stm2@users.sf.net>
2022-01-04 09:53:53 +0100

    WIP 5

--- test_intl.c
+++ test_intl.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 
-#ifdef HAVE_GETTEXT
 #include <locale.h>
 #include <libintl.h>
-#else
-#define gettext(X) (X)
-#define ngettext(S, P, N) (((N) == 1) ? S : P)
-#endif
 
 #define _(String) gettext (String)
 #define gettext_noop(String) String
@@ -14,11 +9,10 @@
 
 int main()
 {
-#ifdef HAVE_GETTEXT
     setlocale(LC_ALL, "");
     bindtextdomain("test_intl", "locale");
     textdomain("test_intl");
-#endif
+
     printf(_("test %s"), "World");
     printf("\n");
     return 0;
